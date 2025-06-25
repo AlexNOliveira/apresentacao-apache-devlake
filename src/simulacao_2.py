428d4554bfb7749efd2d3fd881a40be796635e6f
Carol QA <carol.qa@example.com>
2025-06-25 14:46:24 +0000

    feat: início da simulação 2

--- src/simulacao_2.py
+++ src/simulacao_2.py
@@ -1,4 +1,2 @@
 # Arquivo gerado por Carol QA
-print('Simulação DORA Avançada 2 - Wed Jun 25 14:25:22 UTC 2025')
-# Segunda alteração
-# Terceira modificação
+print('Simulação DORA Avançada 2 - Wed Jun 25 14:46:24 UTC 2025')
