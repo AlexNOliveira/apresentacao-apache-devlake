1ed5cf862df0945b55728408ebd9eb8e1a9a8b62
Alice Dev <alice.dev@example.com>
2025-07-04 12:43:13 +0000

    feat: início da simulação 2

--- src/simulacao_2.py
+++ src/simulacao_2.py
@@ -1,4 +1,2 @@
-# Arquivo gerado por Carol QA
-print('Simulação DORA Avançada 2 - Wed Jun 25 17:02:34 UTC 2025')
-# Segunda alteração
-# Terceira modificação
+# Arquivo gerado por Alice Dev
+print('Simulação DORA Avançada 2 - Fri Jul  4 12:43:13 UTC 2025')
