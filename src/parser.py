14c6244ad77710916ba8ba8d89d65a728ab7771b
Carol QA <carol.qa@example.com>
2025-09-29 12:32:41 +0000

    chore: simulação 3 por Carol QA

--- src/parser.py
+++ src/parser.py
@@ -176,3 +176,5 @@ print('Alteração 1 por Carol QA')
 print('Alteração 2 por Alice Dev')
 # Modificado por Alice Dev em Mon Sep 29 12:32:41 UTC 2025
 print('Alteração 2 por Alice Dev')
+# Modificado por Carol QA em Mon Sep 29 12:32:41 UTC 2025
+print('Alteração 3 por Carol QA')
