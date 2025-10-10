53e9daa9818e73845709289a71adb932026fe9c8
Carol QA <carol.qa@example.com>
2025-10-10 12:31:53 +0000

    chore: simulação 1 por Carol QA

--- src/storage.py
+++ src/storage.py
@@ -228,3 +228,5 @@ print('Alteração 2 por Alice Dev')
 print('Alteração 2 por Alice Dev')
 # Modificado por Bob Ops em Thu Oct  9 12:31:38 UTC 2025
 print('Alteração 3 por Bob Ops')
+# Modificado por Carol QA em Fri Oct 10 12:31:53 UTC 2025
+print('Alteração 1 por Carol QA')
