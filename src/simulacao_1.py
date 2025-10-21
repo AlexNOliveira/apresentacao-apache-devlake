e2cdd32292c66f0ee4a58353918a098b7d0ad1b6
Carol QA <carol.qa@example.com>
2025-10-21 11:01:22 +0000

    feat: simulação 1 por Carol QA

--- src/simulacao_1.py
+++ src/simulacao_1.py
@@ -1,2 +1,2 @@
-# Arquivo gerado por Dan UX
-print('Simulação DORA Avançada 1 - Mon Oct 20 11:00:50 UTC 2025')
+# Arquivo gerado por Carol QA
+print('Simulação DORA Avançada 1 - Tue Oct 21 11:01:22 UTC 2025')
