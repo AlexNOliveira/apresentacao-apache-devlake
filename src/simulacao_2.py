458459fe61120a98ff1993ba11eb27979b2149f1
Carol QA <carol.qa@example.com>
2025-11-03 11:01:07 +0000

    feat: simulação 2 por Carol QA

--- src/simulacao_2.py
+++ src/simulacao_2.py
@@ -1,2 +1,2 @@
-# Arquivo gerado por Alice Dev
-print('Simulação DORA Avançada 2 - Fri Oct 31 11:02:00 UTC 2025')
+# Arquivo gerado por Carol QA
+print('Simulação DORA Avançada 2 - Mon Nov  3 11:01:07 UTC 2025')
